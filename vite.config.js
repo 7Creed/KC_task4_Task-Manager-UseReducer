010271a9af9df601bf17ebf7a277a9539180e3b8
7Creed <4chimabika@gmail.com>
2023-09-17 18:49:09 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/KC_task4_Task-Manager_useReducer/",
+  base: "/KC_task4_Task-Manager_UseReducer/",
   plugins: [react(), tailwindcss()],
 });
